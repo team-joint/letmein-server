a264093e2d3ecdc8092eae2bed7153cfbf0ad89c
su080915 <tbr335@naver.com>
2024-10-01 09:27:32 +0900

    initial commit

---
+++ src/main/java/com/example/letmein/LetmeinApplication.java
@@ -0,0 +1,13 @@
+package com.example.letmein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetmeinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetmeinApplication.class, args);
+    }
+
+}
