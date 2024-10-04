0d8b1c1eb56027b081b3151c78a81ec138f065ac
su080915 <tbr335@naver.com>
2024-10-04 16:54:53 +0900

    feat: swagger setting 1

--- src/main/java/com/example/letmein/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/example/letmein/global/config/swagger/SwaggerConfig.java
@@ -1,4 +1,34 @@
 package com.example.letmein.global.config.swagger;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(
+                        new Info().title("let me in")
+                                .description("let me in API")
+                                .version("1.0.0")
+                )
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                .components(
+                        new Components()
+                                .addSecuritySchemes("Authorization",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                                .in(SecurityScheme.In.HEADER)
+                                                .name("Authorization")
+                                ));
+
+    }
 }
